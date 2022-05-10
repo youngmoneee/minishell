7af26cd03d025532b6849b6e7d5ed4e9a05cd4ed
kimdong <parktest0325@gmail.com>
2022-05-11 01:12:39 +0900

    add debug_print_lst

--- inc/debug.h
+++ inc/debug.h
@@ -6,7 +6,7 @@
 /*   By: dongkim <dongkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 02:30:45 by dongkim           #+#    #+#             */
-/*   Updated: 2022/05/05 02:35:20 by dongkim          ###   ########.fr       */
+/*   Updated: 2022/05/11 01:08:32 by dongkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 # define DEBUG_H
 
 void	debug_print_elems(t_elem *elems, int idx);
+void	debug_print_lst(t_lst *lst, int idx);
 
 #endif
--- srcs/debug.c
+++ srcs/debug.c
@@ -6,7 +6,7 @@
 /*   By: dongkim <dongkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 02:26:47 by dongkim           #+#    #+#             */
-/*   Updated: 2022/05/05 02:36:07 by dongkim          ###   ########.fr       */
+/*   Updated: 2022/05/11 01:11:28 by dongkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,36 @@ void	debug_print_elems(t_elem *elems, int idx)
 	{
 		i = idx;
 		printf("%d)\tstr:%s\n\ttype:%d\n", i + 1, elems[i].data, elems[i].type);
+	}
+}
+
+void	print_node(t_lst *node)
+{
+	int	i;
+
+	i = 0;
+	printf("int argc:\t%d\n", node->argc);
+	printf("char** argv:\t%p\n", node->argv);
+	while (node->argv[i])
+	{
+		printf(" -> [%s]\n", node->argv[i]);
+		i++;
+	}
+	printf("type:\t\t%d\n", node->type);
+	printf("next:\t\t%p\n", node->next);
+	printf("\n");
+}
+
+void	debug_print_lst(t_lst *lst, int idx)
+{
+	int	i;
+
+	i = 0;
+	while (lst)
+	{
+		if (idx < 0 || idx == i)
+			print_node(lst);
+		i++;
+		lst = lst->next;
 	}
 }
